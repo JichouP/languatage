54ef58bd7fd05c4040be8d329887e8de39ec92df
JichouP <akkunn100@gmail.com>
2022-08-18 22:46:10 +0900

    :recycle: refactor

---
+++ src/config.rs
@@ -0,0 +1,49 @@
+use serde::Deserialize;
+
+const DEFAULT_CONFIG: &str = include_str!("config.yaml");
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+pub struct Config {
+    pub language: Vec<LanguageConfigItem>,
+    pub common: CommonConfig,
+}
+
+impl Config {
+    pub fn new(language: Vec<LanguageConfigItem>, common: CommonConfig) -> Self {
+        Self { language, common }
+    }
+
+    pub fn default() -> Self {
+        serde_yaml::from_str(DEFAULT_CONFIG).expect("src/config.yaml is invalid")
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+pub struct LanguageConfigItem {
+    pub lang: String,
+    pub ext: Vec<String>,
+    pub ignore: Vec<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+pub struct CommonConfig {
+    pub ignore: Vec<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_default_test() {
+        let config = Config::default();
+        assert_eq!(
+            config.language[0],
+            LanguageConfigItem {
+                lang: "rust".into(),
+                ext: vec!["rs".into()],
+                ignore: vec!["target".into()]
+            }
+        )
+    }
+}
